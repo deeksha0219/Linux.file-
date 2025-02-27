1dd9f5172479cf14073f354562e48cb995ab52b4
deeksha0219 <deeksha021907@gmail.com>
2025-02-27 11:47:30 +0530

    Add files via upload

---
+++ myscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Open firfox
+firefox https://goggle.com &
+disown
+
+#Open Filemanager
+nautilus &
+disown
+
+#Document viwer
+evince &
+disown
+
+#editor - open gedit
+gedit my_second_script.sh &
+disown
+
+
+
+
