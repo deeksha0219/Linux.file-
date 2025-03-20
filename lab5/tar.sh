35b0936a75a806c865a60fbbf993b0248b0050b0
deeksha0219 <deeksha021907@gmail.com>
2025-03-20 10:22:04 +0530

    tar.sh

--- lab5/tar.sh
+++
@@ -1,9 +0,0 @@
-rvu@rvu-OptiPlex-SFF-7020:~$ cd ~/documents
-bash: cd: /home/rvu/documents: No such file or directory
-rvu@rvu-OptiPlex-SFF-7020:~$ cd ~/Documents
-rvu@rvu-OptiPlex-SFF-7020:~/Documents$ mkdir backup
-rvu@rvu-OptiPlex-SFF-7020:~/Documents$ cp *.c backup/
-rvu@rvu-OptiPlex-SFF-7020:~/Documents$ tar -cf backup.tar.gz backup
-rvu@rvu-OptiPlex-SFF-7020:~/Documents$ rm -r backup
-rvu@rvu-OptiPlex-SFF-7020:~/Documents$ 
-
